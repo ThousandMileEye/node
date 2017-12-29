5ec9905fd9d9cf015b412e5cbf62e3396ed611da
Naoya Sawada <naoya@tuntunkun.com>
2017-12-29 12:36:04 +0900

    add __init__.py

---
+++ node/api/v1/model/__init__.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from base import BaseObject
+from monitoringPoint import MonitoringPoint
+from label import Label, LabelType
+
+__all__ = [
+	BaseObject,
+	MonitoringPoint,
+	Label,
+	LabelType
+]
+
